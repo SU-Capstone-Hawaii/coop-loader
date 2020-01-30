b261eac8c9d81eafda516eac58f7cf58227b4f2f
Jake Ladera <laderah@seattleu.edu>
2020-01-30 12:16:57 -0800

    Added comments

--- NormalizedDB/Maphawks_Db_Handler.py
+++ NormalizedDB/Maphawks_Db_Handler.py
@@ -1,13 +1,29 @@
-import pyodbc
+import pyodbc # python SQL Server database integration library
+
+'''
+Below are Python Class imports. Each class corresponds to a table in the normalized DB schema.
+'''
 from NormalizedDB.Locations_Table import Locations_Table
 from NormalizedDB.Contacts_Table import Contacts_Table
 from NormalizedDB.SpecialQualities_Table import SpecialQualities_Table
 from NormalizedDB.HoursPerDayOfTheWeek_Table import HoursPerDayOfTheWeek_Table
+
+'''
+Enums for SQL Commands (CREATE and INSERT) and SQL Tables (same tables as above) 
+'''
 from Sql_Enums import Sql_Commands, Sql_Tables
-import time
+
+
+import time # used in insert_into_db() so we have some time to see printed message on console
 
 class Maphawks_Db_Handler:
 
+    '''
+    Constructor
+    SUMMARY:
+    Reads db_connection_str.txt (txt file with one line: the db connection string). Creates instances
+    of all SQL Table Classes.
+    '''
     def __init__(self):
         with open('./NormalizedDb/db_connection_str.txt') as file:
             self.connection_str = file.read().strip()
@@ -16,6 +32,10 @@ class Maphawks_Db_Handler:
         self.special_qualities_table = SpecialQualities_Table()
         self.hours_per_day_of_the_week_table = HoursPerDayOfTheWeek_Table()
     
+    '''
+    SUMMARY:
+    Calls execute_sql_command() helper method. Drives the creation of tables in SQL DB.
+    '''
     def create_tables(self):
         # Create tables in DB
         self.execute_sql_command(Sql_Commands.CREATE, Sql_Tables.Locations)
@@ -23,6 +43,11 @@ class Maphawks_Db_Handler:
         self.execute_sql_command(Sql_Commands.CREATE, Sql_Tables.SpecialQualities)
         self.execute_sql_command(Sql_Commands.CREATE, Sql_Tables.HoursPerDayOfTheWeek)
     
+
+    '''
+    SUMMARY:
+    Calls execute_sql_command() helper method. Drives the insertion of location records into the DB.
+    '''
     def insert_into_db(self, locations):
         print("length of locations: ", len(locations))
         time.sleep(3)
@@ -30,8 +55,12 @@ class Maphawks_Db_Handler:
             self.execute_sql_command(Sql_Commands.INSERT, None, location)
     
     '''
+    SUMMARY:
     Determines type of sql command and calls corresponding method.
-    In case of SELECT command, table is an array, otherwise, table can be assumed to refer to only 1 table
+    In case of SELECT command, table is an array, but in all other cases table parameter can be assumed to 
+    refer to only 1 table.
+
+    **SELECT functionality has yet to be implemented**
     '''
     def execute_sql_command(self, cmd_type, table=None, location=None, columns=None):
         if cmd_type is Sql_Commands.CREATE: self.execute_create(table) # Create a new table
@@ -39,6 +68,15 @@ class Maphawks_Db_Handler:
         elif cmd_type is Sql_Commands.SELECT: self.execute_sql_command(table, columns)
         else: print("SQL Command: {} is not supported".format(str(cmd_type)))
 
+    '''
+    SUMMARY:
+    CREATES tables in DB.
+        Step 1: Call table class instance to return table creation SQL statement
+        Step 2: Call commit_command() helper method. commit_command() parameter 'cmd' must be passed as a
+                LIST. This ensures method will work for both single statements as well as with
+                multiple statements.
+        Step 3: Retry table creation if success is not initially achieved.
+    '''
     def execute_create(self, table):
         commit_status = None
         while commit_status is not True:
@@ -50,8 +88,15 @@ class Maphawks_Db_Handler:
                 print("TABLE {} DOES NOT EXIST".format(str(table)))
                 return
             commit_status = self.commit_command([cmd])    
+
     '''
-    Inserts a row for arg:location into all Sql_Tables
+    SUMMARY:
+    Inserts a row for location (parameter) into all Sql_Tables. 
+        Step 1: Gets SQL INSERT statement from Locations Table Class. Returns the SQL statement and the
+                BECU LocationId (GUID)
+        Step 2: Gets SQL INSERT statement from other Table Classes.
+        Step 3: Calls commit_command() to commit commands to DB
+        Step 4: Retry until success.
     '''
     def execute_insert(self, location):
         commit_status = None
@@ -62,6 +107,11 @@ class Maphawks_Db_Handler:
             cmd_hours = self.hours_per_day_of_the_week_table.get_insert_row(location, locationId)
             commit_status = self.commit_command([cmd_locations, cmd_contacts, cmd_special_qualities, cmd_hours])
 
+
+    '''
+    SUMMARY:
+    Commits SQL statements to DB
+    '''
     def commit_command(self, cmds):
         with pyodbc.connect(self.connection_str) as conn:
             cursor = conn.cursor()
